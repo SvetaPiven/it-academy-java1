6dd63b8649a68570f407c79845f52165ed6c0c89
SvetaPiven <svetapiven93@gmail.com>
2022-09-16 11:08:31 +0300

    feat: added 4 Tasks

---
+++ Lesson04/src/Task1.java
@@ -0,0 +1,7 @@
+public class Task1 {
+    public static void main(String[] args) {
+        for (int num = 0; num <= 10; num++) {
+            System.out.println(num);
+        }
+    }
+}
---
+++ Lesson04/src/Task4.java
@@ -0,0 +1,22 @@
+public class Task4 {
+    public static void main(String[] args) {
+        int num1 = 1;
+        String longText = "";
+        while (num1 <= 10) {
+            int num2 = 1;
+            while (num2 <= 10) {
+                int mulTable = num1 * num2;
+                longText = num1 + " * " + num2 + " = " + mulTable;
+                switch (longText.length()) {
+                    case 9 -> System.out.print(longText + "      ");
+                    case 10 -> System.out.print(longText + "     ");
+                    case 11 -> System.out.print(longText + "    ");
+                    case 13 -> System.out.print(longText + "  ");
+                }
+                num2++;
+            }
+            System.out.println();
+            num1++;
+        }
+    }
+}
