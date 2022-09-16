d62a8db3e87ee5c2c92ec6bdea39192ee1eb0295
SvetaPiven <svetapiven93@gmail.com>
2022-09-16 09:35:40 +0300

    feat: changed Task1

--- lesson03/src/Task1.java
+++ lesson03/src/Task1.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-        int num = 7;
+        int num;
         Scanner in = new Scanner(System.in);
         System.out.print("Input num: ");
         num = in.nextInt();
@@ -11,7 +11,7 @@ public class Task1 {
             case 1 -> System.out.println("Monday");
             case 2 -> System.out.println("Tuesday");
             case 3 -> System.out.println("Wednesday");
-            case 4 -> System.out.println("Tuesday");
+            case 4 -> System.out.println("Thursday");
             case 5 -> System.out.println("Friday");
             case 6 -> System.out.println("Saturday");
             case 7 -> System.out.println("Sunday");
